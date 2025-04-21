fd69642fbb4e3c6813f79661ddb49bc26dad6128
jodijonatan <jdi102350@gmail.com>
2025-04-21 08:32:12 +0700

    add php dom

---
+++ PHP-DOM/main.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("toggle");
+btn.style.background = "red";
+btn.style.border = "none";
+btn.style.display = "flex";
+btn.style.margin = "auto";
+btn.style.marginTop = "200px";
+btn.style.padding = "10px 20px";
+btn.style.color = "white";
+btn.style.borderRadius = "20px";
+
+function masuk() {
+  btn.style.background = "blue";
+  btn.style.color = "black";
+}
