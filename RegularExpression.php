45becd783f9c90c4c2dd124da03e3af3039a213e
Jodi Jonatan <jdi102350@gmail.com>
2025-06-08 18:46:34 +0700

    learn exception

---
+++ RegularExpression.php
@@ -0,0 +1,5 @@
+<?php
+
+$result = preg_match_all("/jo|jodi|slebew/i", "Jodi Jonatan Karo karo");
+
+var_dump($result);
--- helper/Validation.php
+++ helper/Validation.php
@@ -5,9 +5,9 @@ function validateLoginRequest (LoginRequest $loginRequest) {
         throw new ValidationException("Username is null");
     } else if (!isset($loginRequest->password)) {
         throw new ValidationException("Password is null");
-    } else if ($loginRequest->username == "") {
+    } else if (trim($loginRequest->username) == "") {
         throw new Exception("Username is blank");
-    } else if ($loginRequest->password == "") {
+    } else if (trim($loginRequest->password) == "") {
         throw new Exception("Password is blank");
     }
 } 
